216e2770db36b45cf8ce25a4383f69da39bce1cc
Juan Camilo Sandoval Devia <juansandoval@rccl.com>
2018-02-27 22:27:07 -0500

    Initial commint in dev_0.0.1 - Icons - Styles - Colors - Animatios - Initial Activity and splash screen

---
+++ app/src/main/java/com/currencymerlin/juansandoval/currency_merlin/view/activity/CurrencyActivity.java
@@ -0,0 +1,30 @@
+package com.currencymerlin.juansandoval.currency_merlin.view.activity;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CurrencyActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected int getLayout() {
+        return 0;
+    }
+
+    @Override
+    protected View getRootView() {
+        return null;
+    }
+
+    @Override
+    protected void inject() {
+
+    }
+
+}
